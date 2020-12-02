2f0a16d38113e57180542b29fb43be5eba735223
Pocas1982 <andres.lemme@gmail.com>
2020-12-02 19:56:09 -0300

    adding json file and axios

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,5 +1,7 @@
-export default function Button({onClick, value, name}){
-  return(
-    <button onClick={onClick} name={name}>{value}</button>
-  )
+export default function Button({ onClick, value, name }) {
+  return (
+    <button onClick={onClick} name={name}>
+      {value}
+    </button>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,28 @@ import Input from "../components/Input";
 import Button from "../components/Button";
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [data, setData] = useState({})
 
- function handleChange(e){
-    setSearchTerm(e.target.value)
+  function handleChange(e) {
+    setSearchTerm(e.target.value);
   }
 
- async function handleClick(){
-   const data = await axios.get(`https://api.github.com/search/users?q=${searchTerm}`)
-   
+  async function handleClick() {
+    const github = await axios.get(
+      `https://api.github.com/users/${searchTerm}`
+    );
 
+    const projectsData = await axios.get('https://api.jsonbin.io/b/5fc817b19abe4f6e7caec5c5/1')
+
+    const userProjects = projectsData.data.find((user) => user.name === searchTerm)
+
+    setData({
+      githubData: github.data
+    })
   }
   return (
     <div>
